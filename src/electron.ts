01d8610c59fd272d1fcdf2019541d968bb61803b
Bulat Cherkashin <bulat.cherkashyn@dewais.com>
2022-01-08 01:42:45 +0200

    fix electron builder

--- src/electron.ts
+++ src/electron.ts
@@ -9,9 +9,10 @@ function createWindow() {
       nodeIntegration: true,
     },
   });
+  win.webContents.openDevTools();
 
   // and load the index.html of the app.
-  win.loadFile('index.html');
+  win.loadFile('dist/index.html');
 }
 app.whenReady().then(() => {
   createWindow();
